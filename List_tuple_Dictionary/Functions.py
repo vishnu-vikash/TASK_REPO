670396aa2deb70a25305915600a1fd45c62c8065
vishnu-vikash <vishnuvikash95@gmail.com>
2025-12-14 10:54:08 +0530

    Functions Learning with new things

--- List_tuple_Dictionary/Functions.py
+++ List_tuple_Dictionary/Functions.py
@@ -13,32 +13,18 @@ x=int(input("enter first number :"))
 y=int(input("enter second number :"))
 addNumb(10,20)
 
-def addition_of_two_numbers(a,b): # a and b are the arguments / inputs which are passed to a function
-    #print(a+b) # printing values/statements on the console
-    return a+b
-x = int(input("Enter first value to be added: ")) # 10 -> '10' -> int('10') -> 10
-# print(type(a))
-# a = int(a) # Type conversion -> It is a way to convert data types of a variable
-# print(type(a))
+def finsPassOrfail(mark):
+    if(mark > 35):
+        print("PASS")
 
-y = int(input("Enter second value to be added: "))
-z = int(input("Enter third value to be added: "))
-print(addition_of_two_numbers(addition_of_two_numbers(x,y),z))
+    else:
+        print("fail")
+a=int(input("enter first number :"))
+finsPassOrfail(a)
 
-def subtract(a,b):
-    print(a-b)
-def add(a,b,c):
-    result=a+b
-    subtract(result,c)
-add(20,30,10)
-
-## closure functions
-
-def add(a,b):
-    result=(a+b)
-    def add2(c):
-        print(f"final Output is {result + c}")
-    return add2( b)
-add(10,5)
-
-
+def printRange(a,b):
+    for i in range(a, b):
+        print(i)
+a= int(input("enter first number :"))
+b= int(input("enter second number :"))
+printRange(a,b)
